8fe2524ea829c9a22761970c12a529f4cd1b4a5a
ratnadek <ratnadeep.konar@gmail.com>
2023-07-19 22:34:50 +0530

    Two Sum | Solution 3

--- TwoSum.js
+++ TwoSum.js
@@ -34,3 +34,25 @@ function twoNumberSum(array, targetSum){
     
     return [];
 }
+
+/*
+    Solution 3 -- Sorting .
+    TC:O(N) SC:O(1)
+*/
+
+function twoNumberSum(array, targetSum){
+    array.sort((a, b) => a - b);
+    let left = 0;
+    let right = array.length - 1;
+    while (left < right) {
+        const currSum = array[left] + array[right];
+        if (currSum === targetSum) {
+        return [array[left], array[right]];
+        } else if(currSum < targetSum) {
+        left++;
+        } else if (currSum > targetSum) {
+        right--;
+        }
+    }
+    return [];
+}
