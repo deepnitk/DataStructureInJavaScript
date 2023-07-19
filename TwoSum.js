6f0b08bf9dc1e0cb2698b58a4d73dc1211f1f5ce
ratnadek <ratnadeep.konar@gmail.com>
2023-07-19 22:15:45 +0530

    Two Sum | solution 2

--- TwoSum.js
+++ TwoSum.js
@@ -15,3 +15,22 @@ function twoNumberSum(array, targetSum){
     }
     return [];
 }
+
+/*
+    Solution 2 -- Hash Table .
+    TC:O(N) SC:O(n)
+*/
+
+function twoNumberSum(array, targetSum){
+    const nums = {};
+    for(const num of array) {
+        const potentialMatch = targetSum - num;
+        if(potentialMatch in nums) {
+            return [potentialMatch, num];
+        } else {
+            nums[num] = true;
+        }
+    }
+    
+    return [];
+}
