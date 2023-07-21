d045e4d5448a23c59ef07421d473f2d3024d396c
ratnadek <ratnadeep.konar@gmail.com>
2023-07-21 23:11:14 +0530

    SortesSquareArray solution 2

--- AlgoExpert/SortedSquareArray.js
+++ AlgoExpert/SortedSquareArray.js
@@ -15,4 +15,33 @@ function sortedSquaredArray(array) {
   
   // Do not edit the line below.
   exports.sortedSquaredArray = sortedSquaredArray;
+
+/*
+    Solution 2 -- Two pointer approach. Check abs value and fill in resultant array from end.
+    TC:O(N) SC:O(N)
+*/
+  function sortedSquaredArray(array) {
+    // Write your code here.
+    const result = new Array(array.length).fill(0);
+    let start = 0;
+    let end = array.length - 1;
+    let j = array.length - 1;
+    while (start <= end) {
+      let a = array[start];
+      let b = array[end];
+      if (Math.abs(a) > Math.abs(b)) {
+        result[j] = a * a;
+        start++;
+      } else {
+        result[j] = b * b;
+        end--;
+      }
+      j--;
+    }
+    return result;
+  }
+  
+  // Do not edit the line below.
+  exports.sortedSquaredArray = sortedSquaredArray;
+  
   
