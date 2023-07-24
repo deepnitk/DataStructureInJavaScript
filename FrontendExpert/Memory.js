6a1f72c85a1dca3b6a12f26f153f2fce68ec1796
ratnadek <ratnadeep.konar@gmail.com>
2023-07-24 21:49:41 +0530

    Memory solution

---
+++ FrontendExpert/Memory.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const TILE_COLORS = ['red', 'green', 'blue', 'yellow'];
+
+export default function Memory() {
+  const [board, setBoard] = useState(() => 
+    shuffle([...TILE_COLORS, ...TILE_COLORS]));
+  const [selectedTiles, setSelectedTiles] = useState([]);
+  const [matchedTiles, setMatchedTiles] = useState([]);
+
+  useEffect(() => {
+    if (selectedTiles.length < 2) return;
+    if (board[selectedTiles[0]] === board[selectedTiles[1]]) {
+      setMatchedTiles([...matchedTiles, ...selectedTiles]);
+      setSelectedTiles([]);
+    } else {
+      const timeOutID = setTimeout(() => setSelectedTiles([]), 1000);
+      return () => clearTimeout(timeOutID);
+    }
+  }, [selectedTiles]);
+  const selectTile = (index) => {
+    if (selectedTiles.length >= 2 || selectedTiles.includes(index)) return;
+    setSelectedTiles([...selectedTiles, index]);
+  }
+  const restartGame = () => {
+    setMatchedTiles([]);
+    setBoard(shuffle([...TILE_COLORS, ...TILE_COLORS]));
+    setSelectedTiles([]);
+  }
+  const didPlayerWin = matchedTiles.length === board.length;
+  
+  return (
+    <>
+      <h1>{didPlayerWin ? 'You Win!' : 'Memory'}</h1>
+      <div className="board">
+        {board.map((tileColor, i) => {
+          const isTurnedOver =
+            selectedTiles.includes(i) || matchedTiles.includes(i);
+            const className = isTurnedOver ? `tile ${tileColor}` : 'tile';
+            return (
+              <div key={i} className={className} onClick={() => selectTile(i)}/>
+            )
+        })}
+      </div>
+      {didPlayerWin && <button onClick={restartGame}>Restart</button>}
+    </>
+  );
+}
+
+/**
+ * Returns the array shuffled into a random order.
+ * Do not edit this function.
+ */
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+
+    // Swap the elements at i and randomIndex
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+  return array;
+}
