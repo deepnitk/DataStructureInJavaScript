7c88a29733d27b86996c76425676a76d24445b56
ratnadek <Ratnadeep.Konar@amdocs.com>
2023-09-07 13:32:53 +0530

    Added eventTarget problem

---
+++ FrontendExpert/JS_Practice/EventTraget.js
@@ -0,0 +1,27 @@
+class EventTarget {
+  // name --> callback functions --> add/remove from datastructure.
+  // We can use set here instead of array
+  constructor() {
+    this.listeners = {};
+  }
+
+  addEventListener(name, callback) {
+    if(!this.listeners.hasOwnProperty(name)) {
+      this.listeners[name] = new Set();
+    }
+    this.listeners[name].add(callback);
+  }
+
+  removeEventListener(name, callback) {
+    this.listeners[name]?.delete(callback);
+  }
+
+  dispatchEvent(name) {
+    this.listeners[name]?.forEach(callback => {
+      callback();
+    });
+  }
+}
+
+// Do not edit the line below.
+exports.EventTarget = EventTarget;
