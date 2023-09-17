39abc8de2562bb9e5dc54ee3c68b0f90f8d4689f
ratnadek <Ratnadeep.Konar@amdocs.com>
2023-09-17 18:50:00 +0530

    Promise.all polyfills

---
+++ FrontendExpert/Frontend Interview Questions | JS Cafe/Polyfills/poltfillPromiseAll.js
@@ -0,0 +1,41 @@
+/**
+ * Problem statement:
+ * Promise.all(array of tasks/API calls).then((data1, data2, data3, ....))
+ *        .catch((error) =>{
+ *              console.error(error
+ *        }
+ * we need to create our own vewrsion of Promise.all
+ *
+ */
+
+const dummyAPI = (time) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(time);
+        }, time)
+    })
+};
+
+const tasksArray = [dummyAPI(1000), dummyAPI(3000), dummyAPI(5000)];
+
+const promisePollyfill = (tasksArray) => {
+    return new Promise((resolve, reject) => {
+        const output = [];
+        tasksArray.forEach((promsie, task) => {
+            promsie.then((data) => {
+                output[index] = data;
+                if (index === tasksArray.length - 1) {
+                    resolve(output);
+                }
+            }).catch((err) => {
+                reject(err);
+            })
+        })
+    })
+}
+
+promisePollyfill(tasksArray).then((data) => {
+    console.log(`output: ${data}`);
+}).catch((err) => {
+    console.log(`err: ${err}`);
+});
